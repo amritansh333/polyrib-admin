9218221cf689443d6dd7c1e095b59caf5af66c9d
Amritansh Jaiswal <153156600+amritansh333@users.noreply.github.com>
2026-08-21 17:46:01 +0530

    Update BrandListPage.tsx

--- src/pages/BrandListPage.tsx
+++ src/pages/BrandListPage.tsx
@@ -14,6 +14,7 @@ import { useToast } from '../providers/ToastProvider';
 import { resources } from '../services/mockData';
 import type { DataEntity } from '../types/admin';
 
+
 export default function BrandListPage() {
   const navigate = useNavigate();
   const toast = useToast();
